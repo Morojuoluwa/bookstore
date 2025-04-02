8628f84bb65b28131c3733cd2012e01887343041
abimbola patrick <abimbolapatrickmj@gmail.com>
2025-04-02 15:51:30 +0100

    implemented the signup authentication on the frontend

--- mobile/app/(auth)/signup.jsx
+++ mobile/app/(auth)/signup.jsx
@@ -1,20 +1,25 @@
-import { View, Text, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { View, Text, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, ActivityIndicator, Alert } from 'react-native'
 import React, { useState } from 'react'
 import styles from "../../assets/styles/signup.styles"
 import COLORS from '../../constants/colors'
 import { Ionicons } from "@expo/vector-icons"
 import { useRouter} from "expo-router"
+import { useAuthStore } from '../../store/authStore'
 
 export default function signup() {
   const [username, setUsername] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [showPassword, setShowPassword] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  // const [isLoading, setIsLoading] = useState(false)
 
+  const { user, isLoading,register} = useAuthStore()
   const router = useRouter()
 
-  const handleSignup = () =>{}
+  const handleSignup = async() =>{
+    const result = await register(username, email, password)
+    if(!result.success) Alert.alert("Error",result.error)
+  }
   return (
     <KeyboardAvoidingView style={{flex:1}} behavior={Platform.OS==='ios'?"padding":'height'}>
       <View style={styles.container}>
@@ -29,7 +34,7 @@ export default function signup() {
               <Text style={styles.label}> Username</Text>
               <View style={styles.inputContainer}>
                 <Ionicons name="person-outline" size={20} color={COLORS.primary} style={styles.inputIcon}/>
-                <TextInput style={styles.input} placeholder='John Doe' placeholderTextColor={COLORS.placeholderText} value={username} onChangeText={setUsername} autoCapitalize='none'/>
+                <TextInput style={styles.input} placeholder='JohnDoe' placeholderTextColor={COLORS.placeholderText} value={username} onChangeText={setUsername} autoCapitalize='none'/>
               </View>
             </View>
 
